2bedbe7e7ecc602af03ac6fcf5a0930fe525e10d
kimberly <kimberly.kung@gmail.com>
2021-08-13 15:35:28 -0700

    Load Balanced

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ module.exports = {
     sidebar: 'http://localhost:3004',
   },
   production: {
-    courseContent: 'http://localhost:3003',
+    courseContent: 'http://ec2-3-226-122-223.compute-1.amazonaws.com',
     reviews: 'http://localhost:3001',
     author: 'http://localhost:3002',
     sidebar: 'http://34.212.83.37:3004',
